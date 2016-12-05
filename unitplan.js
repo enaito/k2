df30dcb04b320e16a89e83c320c8f4258d77ff19
enaito <emily.naito@tufts.edu>
2016-12-04 22:25:28 -0500

    remove old unit plan

--- unitplan.js
+++ unitplan.js
@@ -1,40 +1,11 @@
-function dayClick(day) {
-    $title = $(day).next();
-    $title.slideToggle(500);
-}
-
-function buttonMove() {
-    var element = $('.box'),
-        originalY = element.offset().top;
-    var topMargin = 20;
-
-    $(window).on('scroll', function(event) {
-        var scrollTop = $(window).scrollTop();
-    
-        element.stop(false, false).animate({
-            top: scrollTop < originalY
-                ? 0
-                : scrollTop - originalY + topMargin
-        }, 175);
-    });
-}
-
 $(document).ready(function() {
-    $(".title").on('click', function () {
-        dayClick(this);
-    });
+    var element = $('.box');
+    var originalY = element.offset().top;
 
-    $('.box').on("click", function() { 
-        $(".title").each(function(i) {
-            dayClick(this);
-        });
-        if( $('.box').text() == "Expand All") {
-            $('.box').text("Contract All");
-        }
-        else {
-            $('.box').text("Expand All");
+    $(window).on('scroll', function() {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop > originalY) {
+            $(element).css("top", scrollTop-originalY);
         }
     });
-
-    buttonMove();
 });
